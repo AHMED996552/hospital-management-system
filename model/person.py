b0915af4b94effc6901f10a1c07f4e3f742aea0c
samirejust <samir.120230292@ejust.edu.eg>
2025-12-19 14:24:57 +0200

    create person and doctor

---
+++ model/person.py
@@ -0,0 +1,13 @@
+
+class Person:
+    def __init__(self, name, age, gender):
+
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def show_details(self):
+
+        print(f"Name : {self.name}")
+        print(f"Age : {self.age}")
+        print(f"Gender : {self.gender}")
